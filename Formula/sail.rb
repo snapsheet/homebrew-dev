8fcf4a7225f207736327069d725e94fa832209b8
Ray Olivero <ray.olivero@snapsheet.me>
2022-03-21 10:57:12 -0500

    Adding some initial bits...

--- Formula/sail.rb
+++ Formula/sail.rb
@@ -3,14 +3,9 @@ require_relative "lib/sail/private_strategy"
 class Sail < Formula
   desc "Lighting-fast cli middleware for Docker containers"
   homepage "https://github.com/bodyshopbidsdotcom/sail"
-  url "https://github.com/bodyshopbidsdotcom/sail/releases/download/v1.0.5/sail-1.0.5.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "462013ed3b8b96b7bdd9194c45ea7f36fd0eeea6727a05d499fd3168fd7e51e1"
+  url "https://github.com/bodyshopbidsdotcom/sail/releases/download/v1.0.6/sail-1.0.6.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "fca6cfc5327a4e63ab951090208b232c9d605f38dcf4747f8f3ccd870aeeb800"
   license "MIT"
-
-  depends_on "jq"
-  depends_on "coreutils"
-  depends_on "python-yq"
-  depends_on "awscli" => "2"
 
   def install
     # Move everything under #{libexec}/
