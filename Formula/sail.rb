398f3d01d67057a51736aedbf11fa8f1bbd7c5cb
Ray Olivero <ray.olivero@snapsheet.me>
2022-03-21 12:17:04 -0500

    Implementing Total Abstraction

--- Formula/sail.rb
+++ Formula/sail.rb
@@ -3,8 +3,8 @@ require_relative "lib/sail/private_strategy"
 class Sail < Formula
   desc "Lighting-fast cli middleware for Docker containers"
   homepage "https://github.com/bodyshopbidsdotcom/sail"
-  url "https://github.com/bodyshopbidsdotcom/sail/releases/download/v1.0.6/sail-1.0.6.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "fca6cfc5327a4e63ab951090208b232c9d605f38dcf4747f8f3ccd870aeeb800"
+  url "https://github.com/bodyshopbidsdotcom/sail/releases/download/v1.0.7/sail-1.0.7.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "76583cbba2a9402fb119d74fc1b6d6e21f50cace9f4fd00549979a148eac6522"
   license "MIT"
 
   def install
