66f26d0b3f2f0c548ff6ad0ec6e76d0c71d71708
Ray Olivero <ray.olivero@snapsheet.me>
2022-03-25 13:19:36 -0500

    Implementing sailOS

--- Formula/sail.rb
+++ Formula/sail.rb
@@ -3,8 +3,8 @@ require_relative "lib/sail/private_strategy"
 class Sail < Formula
   desc "Lighting-fast cli middleware for Docker containers"
   homepage "https://github.com/bodyshopbidsdotcom/sail"
-  url "https://github.com/bodyshopbidsdotcom/sail/releases/download/v1.0.14/sail-1.0.14.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "cc7db3a8824deb930727518d21f070766f2d30292a2b0c893227d9154946164e"
+  url "https://github.com/bodyshopbidsdotcom/sail/releases/download/v1.0.15/sail-1.0.15.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "ea2ce895e637a0859d8ecdcc26e10d268397bd529e7a42d3f297d3246239628a"
   license "MIT"
 
   def install
